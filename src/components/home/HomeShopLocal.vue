5fd6808bbb6ec517330f54d9bdca1bac5d8cf735
Josue Diaz <jdiaz77711@gmail.com>
2020-07-30 09:58:44 +0100

    add style for local section

--- src/components/home/HomeShopLocal.vue
+++ src/components/home/HomeShopLocal.vue
@@ -1,34 +1,42 @@
 <template>
   <section class="home-local container">
+    <div class="home-local__spacer"></div>
     <div class="home-local__image-container">
-      <img
-        class="home-local__featured-image"
-        style="width:400px;"
-        :src="localData.featured_image"
-        alt="shop local image"
-      />
+      <div>
+        <img
+          class="home-local__featured-image"
+          :src="localData.featured_image"
+          alt="shop local image"
+        />
+      </div>
     </div>
     <div class="home-local__description-container">
-      <h2 class="home-local__description-title">
-        {{ localData.title }}
-      </h2>
-      <p
-        class="home-local__paragraph"
-        v-for="(p, i) in localData.descriptions"
-        :key="i"
-      >
-        {{ p }}
-      </p>
-      <p>--------</p>
-      <p>
-        <span v-for="(owner, i) in localData.owners" :key="i"
-          >{{ owner }}
-          <span v-if="i !== localData.owners.length - 1">{{ "&" }}</span></span
+      <div class="home-local__description-content">
+        <h2 class="home-local__description-title font-roboto-medium">
+          {{ localData.title }}
+        </h2>
+        <p
+          class="home-local__paragraph"
+          v-for="(p, i) in localData.descriptions"
+          :key="i"
         >
-      </p>
-      <p>
-        <b>{{ localData.company }}</b>
-      </p>
+          {{ p }}
+        </p>
+        <p class="home-local__separator">--------</p>
+        <p>
+          <span v-for="(owner, i) in localData.owners" :key="i"
+            >{{ owner }}
+            <span
+              v-if="i !== localData.owners.length - 1"
+              class="home-local__horizontal-separator"
+              >{{ "&" }}</span
+            ></span
+          >
+        </p>
+        <p>
+          <b class="font-roboto-medium">{{ localData.company }}</b>
+        </p>
+      </div>
     </div>
   </section>
 </template>
